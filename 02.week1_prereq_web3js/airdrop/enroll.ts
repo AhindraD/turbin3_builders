df93768432f368a865c30233fbb144f25cc11b0d
Ahindra D. <official.ahindra@gmail.com>
2025-01-08 15:08:32 +0530

    idl

---
+++ 02.week1_prereq_web3js/airdrop/enroll.ts
@@ -0,0 +1,3 @@
+import { Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor"
+import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
