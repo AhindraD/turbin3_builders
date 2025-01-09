aacf3776720e49a8475b329c260b287301d08e00
Ahindra D <official.ahindra@gmail.com>
2025-01-09 22:04:15 +0530

    keypair

--- 02.week1_prereq_web3js/enrollment_dapp/src/lib.rs
+++ 02.week1_prereq_web3js/enrollment_dapp/src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solana_sdk::{
+    pubkey::Pubkey,
+    signature::{Keypair, Signer},
+};
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn keygen() {
+        let dev_kp = Keypair::new();
+        println!("New wallet created: {}", dev_kp.pubkey().to_string());
+        println!("----------------------------------------------------");
+        println!("Private key as bytes array: ");
+        println!("{:?}", dev_kp.to_bytes());
     }
+
+    #[test]
+    fn airdrop() {}
+
+    #[test]
+    fn transfer_sol() {}
 }
