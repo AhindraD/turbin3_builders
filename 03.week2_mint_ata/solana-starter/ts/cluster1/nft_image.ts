4afadc605e65a7886d26a85da2003837a4eec174
Ahindra D <official.ahindra@gmail.com>
2025-01-17 00:40:01 +0530

    nft_image_and_json_uri

--- 03.week2_mint_ata/solana-starter/ts/cluster1/nft_image.ts
+++ 03.week2_mint_ata/solana-starter/ts/cluster1/nft_image.ts
@@ -1,8 +1,9 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
 import { readFile } from "fs/promises"
+import path from "path"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -10,21 +11,31 @@ const umi = createUmi('https://api.devnet.solana.com');
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
+const file_path = path.resolve(__dirname, "assets/master-chef.png");
+
 umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
     try {
         //1. Load image
+        const image = await readFile(file_path);
         //2. Convert image to generic file.
+        const nft_image = createGenericFile(image, "master-chef-nft.png",
+            {
+                displayName: "Master Chef NFT",
+                contentType: "image/png"
+            });
         //3. Upload image
 
         // const image = ???
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        const [myUri] = await umi.uploader.upload([nft_image]);
+        const correct_uri = myUri.replace("https://arweave.net/", "https://devnet.irys.xyz/");
+        console.log("Your image URI: ", correct_uri);
+        //"https://devnet.irys.xyz/ELkWp6e2wodfCiJQ9avnzLDd8VYkqiLys8e9ZyByHqbL
     }
-    catch(error) {
+    catch (error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
--- 03.week2_mint_ata/solana-starter/ts/cluster1/nft_metadata.ts
+++ 03.week2_mint_ata/solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,7 +1,8 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { createGenericFile, createSignerFromKeypair, percentAmount, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import { json } from "stream/consumers";
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -17,29 +18,39 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        const image = "https://devnet.irys.xyz/ELkWp6e2wodfCiJQ9avnzLDd8VYkqiLys8e9ZyByHqbL"
+        const metadata = {
+            name: "Master Chef Spartan NFT",
+            symbol: "HALO",
+            description: "A Spartan NFT, the first of its kind. Build for Turbin3 Q1 2025 Builders' Cohort.",
+            image: image,
+            external_url: "https://github.com/AhindraD/turbin3_builders",
+            attributes: [
+                { trait_type: "Level", value: 'Master' },
+                { trait_type: "Class", value: 'Spartan' },
+                { trait_type: "Team", value: 'Turbin3' }
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: "https://devnet.irys.xyz/ELkWp6e2wodfCiJQ9avnzLDd8VYkqiLys8e9ZyByHqbL"
+                    },
+                ],
+                category: "image",
+            },
+            creators: [{
+                address: "5QQXFaZX9mmmGGqEcZSPFKgfr18BdHyvjfVys2DYLk3E",
+                share: 333,
+                verified: true
+            }]
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        const correct_uri = myUri.replace("https://arweave.net/", "https://devnet.irys.xyz/");
+        console.log("Your metadata URI: ", correct_uri);
+        // https://devnet.irys.xyz/BggFib5qQazYM6sT3UJXAVHdfS4BkE9Uoav6WZQrCdqj
     }
-    catch(error) {
+    catch (error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
