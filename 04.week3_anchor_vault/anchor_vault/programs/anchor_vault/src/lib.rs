ce1dd411d5866b5cb48e598ffe58239db86db246
Ahindra D <official.ahindra@gmail.com>
2025-01-22 07:56:50 +0530

    deposit_to_vault

--- 04.week3_anchor_vault/anchor_vault/programs/anchor_vault/src/lib.rs
+++ 04.week3_anchor_vault/anchor_vault/programs/anchor_vault/src/lib.rs
@@ -1,4 +1,9 @@
-use anchor_lang::prelude::*;
+use std::io::Result;
+
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
 
 declare_id!("AsaWZFHPYynJtnp2r5zqaksCEi7ufLv8ECDbo9zeZafL");
 
@@ -42,4 +47,46 @@ pub struct Initialize<'info> {
     pub vault: SystemAccount<'info>,
 
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: &InitializeBumps) -> Result<()> {
+        self.state.vault_bump = bumps.vault;
+        self.state.state_bump = bumps.state;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Payments<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        seeds=[b"state",user.key().as_ref()],
+        bump=state.state_bump,
+    )]
+    pub state: Account<'info, VaultState>,
+
+    #[account(
+        mut,
+        seeds=[b"vault",state.key().as_ref()],
+        bump=state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Payments<'info> {
+    pub fn deposit(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+        let cpi_accounts = Transfer {
+            from: self.user.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        let _ = transfer(cpi_ctx, amount);
+        Ok(())
+    }
 }
