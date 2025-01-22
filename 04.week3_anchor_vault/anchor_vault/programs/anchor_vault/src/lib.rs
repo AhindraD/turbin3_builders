689846bb20198fbf6897f6ecff6adb871151ad42
Ahindra D <official.ahindra@gmail.com>
2025-01-22 08:54:01 +0530

    vault-drain-instruction

--- 04.week3_anchor_vault/anchor_vault/programs/anchor_vault/src/lib.rs
+++ 04.week3_anchor_vault/anchor_vault/programs/anchor_vault/src/lib.rs
@@ -1,5 +1,3 @@
-use std::io::Result;
-
 use anchor_lang::{
     prelude::*,
     system_program::{transfer, Transfer},
@@ -104,6 +102,49 @@ impl<'info> Payments<'info> {
 
         let cpi_ctx = CpiContext::new_with_signer(cpi_program, ctx_accounts, signer_seeds);
         let _ = transfer(cpi_ctx, amount);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        close=user,
+        seeds=[b"state",user.key().as_ref()],
+        bump=state.state_bump,
+    )]
+    pub state: Account<'info, VaultState>,
+
+    #[account(
+        mut,
+        seeds=[b"vault",state.key().as_ref()],
+        bump=state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let total_balance = self.vault.get_lamports();
+
+        let state_key = self.state.to_account_info().key();
+        let seeds = &[b"vault", state_key.as_ref(), &[self.state.vault_bump]];
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        let _ = transfer(cpi_ctx, total_balance);
         Ok(())
     }
 }
